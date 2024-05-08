f9080f6718dd47c5995030856ab23da63a1b2ff8
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-05-08 20:29:08 +0900

    失敗するケースで書いてしまっていたので修正

--- playwright/test_example.py
+++ playwright/test_example.py
@@ -6,7 +6,7 @@ def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
 
     # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("hono"))
+    expect(page).to_have_title(re.compile("Playwright"))
 
 
 def test_get_started_link(page: Page):
