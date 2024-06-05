205ff353f0d91211a9640968411387a308d1d415
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-06-05 09:12:02 +0900

    chunksを知ったので、コード例を追加

--- django-csv-upload/mysite/supply/views.py
+++ django-csv-upload/mysite/supply/views.py
@@ -12,18 +12,20 @@ class UploadCSVView(View):
 
     def post(self, request):
         csv_file = request.FILES['csv_file']
-        csv_file_data = self.read_csv_file(csv_file)
-        self.save_to_db(csv_file_data)
+        # csv_file_data = self.read_csv_file(csv_file)
+        # csv_file_data = self.chunks_csv(csv_file)
+        self.chunks_csv(csv_file)
+        # self.save_to_db(csv_file_data)
         messages.success(request, 'CSV upload successful!')
         return render(request, self.template_name)
 
-    def read_csv_file(self, file):
-        print(type(file))  # <class 'django.core.files.uploadedfile.InMemoryUploadedFile'>
-        csv_file_data = file.read().decode('utf-8').splitlines()
-        print(type(csv_file_data))  # <class 'list'>
-        reader = csv.DictReader(csv_file_data)
-        print(type(reader))  # <class 'csv.DictReader'>
-        return reader
+    # def read_csv_file(self, file):
+    #     print(type(file))  # <class 'django.core.files.uploadedfile.InMemoryUploadedFile'>
+    #     csv_file_data = file.read().decode('utf-8').splitlines()
+    #     print(type(csv_file_data))  # <class 'list'>
+    #     reader = csv.DictReader(csv_file_data)
+    #     print(type(reader))  # <class 'csv.DictReader'>
+    #     return reader
 
     def save_to_db(self, reader):
         for row in reader:
@@ -34,3 +36,24 @@ class UploadCSVView(View):
                 price=row['価格'],
                 quantity=row['数量']
             )
+
+    def save_to_db2(self, row):
+        Supply.objects.update_or_create(
+            name=row['名前'],
+            price=row['価格'],
+            quantity=row['数量']
+        )
+
+
+    def chunks_csv(self, file):
+        from io import StringIO
+        for chunk in file.chunks():
+            print(chunk)
+            print(type(chunk))  # bytes
+            decoded_chunk = chunk.decode('utf-8')
+            print(decoded_chunk)
+            print(type(decoded_chunk))  # str
+            reader = csv.DictReader(StringIO(decoded_chunk))
+            for row in reader:
+                print(row)
+                self.save_to_db2(row)
