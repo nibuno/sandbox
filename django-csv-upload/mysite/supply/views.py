ac5d512095110480fac7a0834def9e6607eb8f61
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-06-06 08:08:32 +0900

    型を綺麗に（？）記述

--- django-csv-upload/mysite/supply/views.py
+++ django-csv-upload/mysite/supply/views.py
@@ -1,8 +1,13 @@
 import csv
-from django.views import View
-from django.shortcuts import render, redirect
+from csv import DictReader
+
 from django.contrib import messages
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.shortcuts import render
+from django.views import View
+
 from .models import Supply
+
 
 class UploadCSVView(View):
     template_name = 'upload_csv.html'
@@ -12,25 +17,18 @@ class UploadCSVView(View):
 
     def post(self, request):
         csv_file = request.FILES['csv_file']
-        # csv_file_data = self.read_csv_file(csv_file)
-        # csv_file_data = self.chunks_csv(csv_file)
         self.chunks_csv(csv_file)
-        # self.save_to_db(csv_file_data)
         messages.success(request, 'CSV upload successful!')
         return render(request, self.template_name)
 
-    # def read_csv_file(self, file):
-    #     print(type(file))  # <class 'django.core.files.uploadedfile.InMemoryUploadedFile'>
-    #     csv_file_data = file.read().decode('utf-8').splitlines()
-    #     print(type(csv_file_data))  # <class 'list'>
-    #     reader = csv.DictReader(csv_file_data)
-    #     print(type(reader))  # <class 'csv.DictReader'>
-    #     return reader
+    def read_csv_file(self, file: InMemoryUploadedFile):
+        csv_file_data: list = file.read().decode('utf-8').splitlines()
+        reader: DictReader = csv.DictReader(csv_file_data)
+        return reader
 
     def save_to_db(self, reader):
         for row in reader:
-            print(row)  # dict
-            print(type(row))
+            # rowのtypeはdict
             Supply.objects.update_or_create(
                 name=row['名前'],
                 price=row['価格'],
@@ -48,11 +46,8 @@ class UploadCSVView(View):
     def chunks_csv(self, file):
         from io import StringIO
         for chunk in file.chunks():
-            print(chunk)
-            print(type(chunk))  # bytes
-            decoded_chunk = chunk.decode('utf-8')
-            print(decoded_chunk)
-            print(type(decoded_chunk))  # str
+            # bytesからstrに変換
+            decoded_chunk: str = chunk.decode('utf-8')
             reader = csv.DictReader(StringIO(decoded_chunk))
             for row in reader:
                 print(row)
