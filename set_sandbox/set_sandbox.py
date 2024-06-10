fa5b723aa9177b5145289f269598b7ef7d1b336f
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-06-10 21:00:30 +0900

    differenceについてのコードも追加

--- set_sandbox/set_sandbox.py
+++ set_sandbox/set_sandbox.py
@@ -9,4 +9,9 @@ bar = {'ボルト', 'ナット', 'トラス小ねじ'}
 # 差集合 = foo に存在して bar には存在しない要素のこと
 baz = foo - bar
 
+# difference を利用しても良いらしい
+# https://docs.python.org/ja/3/library/stdtypes.html#frozenset.difference
+# こちらだと、他にも集合を加えられるみたいだった(*others)となっていたので
+# baz = foo.difference(bar)
+
 print(baz)  #  {'ねじ'}
