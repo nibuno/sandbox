3c7deb417cf289732720bbe52545e555477d7f84
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-06-14 08:00:01 +0900

    引数を追加

--- django-csv-upload/mysite/supply/forms.py
+++ django-csv-upload/mysite/supply/forms.py
@@ -3,4 +3,6 @@ from django import forms
 class UploadCSVForm(forms.Form):
     csv_file = forms.FileField(
         label="CSVファイル",  # 入力すると文字列をHTML側で表示できる
+        max_length=1000,  # 最大文字数を制限できる（ファイルだとあまり意味がない？）
+        allow_empty_file=False,  # 空ファイルを許可しない
     )
