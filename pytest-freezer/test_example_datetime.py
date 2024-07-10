1fc935ada25d483af371573e352f9707a1564b30
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-07-11 08:12:26 +0900

    失敗例（FakeDateTimeとなる）を記述

--- pytest-freezer/test_example_datetime.py
+++ pytest-freezer/test_example_datetime.py
@@ -11,3 +11,16 @@ def test_get_current_time(freezer):
 
     # assert
     assert actual == expected
+
+def test_fail_get_current_time(freezer):
+    # arrange
+    freezer.move_to("2023-01-01 12:00:00")
+
+    expected = datetime(year=2024, month=1, day=1, hour=12, minute=0)
+
+    # act
+    actual = datetime.now()
+
+    # assert
+    # AssertionError: assert FakeDatetime(2023, 1, 1, 12, 0) == FakeDatetime(2024, 1, 1, 12, 0) となる
+    assert actual == expected
