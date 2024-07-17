239639a7f58d1d81b725fabe67b80301a72d0f2e
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-07-17 20:28:14 +0900

    typingについてサンプルコード記述

--- literal/example-literal.py
+++ literal/example-literal.py
@@ -2,14 +2,24 @@
 https://docs.python.org/ja/3/library/typing.html#typing.Literal
 """
 
-from typing import Any, Literal
+from typing import Any, Literal, TypeAlias
 
 def validate_simple(data: Any) -> Literal[True]:  # always returns True
-    ...
+    return True
 
+# Python3.12で追加された書き方
+# ただし、mypy側では未対応な様子
+# https://stackoverflow.com/questions/78311150/mypy-error-pep-695-type-aliases-are-not-yet-supported
 type Mode = Literal['r', 'rb', 'w', 'wb']
+
+# mypy1.10.1で対応するための書き方その1
+# Mode = Literal['r', 'rb', 'w', 'wb']
+
+# mypy1.10.1で対応するための書き方その2
+# TypeAliasでもOK
+# Mode: TypeAlias = Literal['r', 'rb', 'w', 'wb']
 def open_helper(file: str, mode: Mode) -> str:
-    ...
+    return "True"
 
 open_helper('/some/path', 'r')      # Passes type check
 open_helper('/other/path', 'typo')  # Error in type checker
