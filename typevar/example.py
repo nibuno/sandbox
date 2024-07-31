cb9b689d064d511f0701d6a0a4c881b442820567
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-08-01 07:53:56 +0900

    TypeVarの例を追加

--- typevar/example.py
+++ typevar/example.py
@@ -6,3 +6,9 @@ from typing import TypeVar
 # 一番シンプルな定義
 # Can be anything とのことで、なんでもあり得るということ
 T = TypeVar("T")
+
+# Can be any subtype of str とのこと
+S = TypeVar("S", bound=str)
+
+# Must be exactly str or bytes とのこと
+A = TypeVar("A", str, bytes)
