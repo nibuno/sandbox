0808c1a673b0c955d4f797d8e627d6a9be1c05be
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-08-20 20:11:04 +0900

    コメント追加

--- randoms/randoms.py
+++ randoms/randoms.py
@@ -30,4 +30,5 @@ random.choice(seq=[1, 2, 3, 4, 5])
 
 # random.choices
 # populationから重複ありでk個選び、リストで返す
-random.choices(population=[1, 2, 3, 4, 5], weights=None, cum_weights=None, k=3)
+# weightsで重みを指定でき、同じ番号の要素に対してより「出やすく」できる
+random.choices(population=[1, 2, 3, 4, 5], weights=[10, 1, 1, 1, 1], cum_weights=None, k=3)
