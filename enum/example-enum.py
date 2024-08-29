aaa560a2e212c899f7a1932be8433b0a92e1d039
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-08-29 20:45:48 +0900

    全て大文字にしなくてよさそう（どちらでもよさそうだが）なので修正

--- enum/example-enum.py
+++ enum/example-enum.py
@@ -1,11 +1,11 @@
 from enum import Enum
 from typing import Literal
-class COLOR(Enum):
+class Color(Enum):
     RED = "red"
     GREEN = "green"
     BLUE = "blue"
 
-colors = Literal[COLOR.RED, COLOR.GREEN, COLOR.BLUE]
+colors = Literal[Color.RED, Color.GREEN, Color.BLUE]
 
 def process_color(color: colors) -> None:
     pass
@@ -13,9 +13,9 @@ def process_color(color: colors) -> None:
 red = "red"
 
 # color型
-enum_red = COLOR.RED
+enum_red = Color.RED
 
 # str
-enum_red_value = COLOR.RED.value
+enum_red_value = Color.RED.value
 
 process_color(color=red)  # Error: Argument 1 to "process_color" has incompatible type "str"; expected "Literal[COLOR.RED, COLOR.GREEN, COLOR.BLUE]"
