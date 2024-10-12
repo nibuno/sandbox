55ebc1b2660a8a5f93522a9d1b984221cb9d6866
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-10-12 21:44:04 +0900

    timedeltaを記述

--- time-calc/example.py
+++ time-calc/example.py
@@ -1,7 +1,20 @@
 # 時間の計算を行う
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # 現在の日時を取得
 # datetime.datetime(2024, 10, 6, 21, 30, 30, 405509) のようなdatetimeオブジェクトが返る
 now = datetime.now()
+
+# timedelta
+# datetimeまたはdateのインスタンスの差（経過時間）を表す
+delta = timedelta(
+    days=1,
+    seconds=2,
+    microseconds=3,
+    milliseconds=4,
+    minutes=5,
+    hours=6,
+    weeks=7
+)
+delta  # datetime.timedelta(days=50, seconds=21902, microseconds=4003)
