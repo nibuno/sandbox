5eeea44789bbec4c624d8b41ec7831911df74789
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-10-16 22:25:08 +0900

    dateオブジェクトのtodayを記載

--- time-calc/example.py
+++ time-calc/example.py
@@ -1,6 +1,6 @@
 # 時間の計算を行う
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
 # 現在の日時を取得
 # datetime.datetime(2024, 10, 6, 21, 30, 30, 405509) のようなdatetimeオブジェクトが返る
@@ -25,3 +25,7 @@ today = datetime.today()
 # e.g. datetime.datetime(2024, 10, 15, 22, 19, 15, 871221)
 # 1日`だけ`加算されている
 tomorrow = today + timedelta(days=1)
+
+# dateでtoday
+today = date.today()
+# e.g. datetime.date(2024, 10, 16)
